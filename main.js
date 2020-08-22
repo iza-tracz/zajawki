3cb41bacbaa745b959d8c4a77a4997daed7531a3
Izabela Tracz <izabelatracz@gmail.com>
2020-08-22 13:38:15 +0200

    before first publication on git hub

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
-const button = document.querySelector('.action--js');
-
-console.log(button)
-
-button.addEventListener('mouseenter', () => {
-  const heading = document.querySelector('.article__heading--js');
-  heading.innerHTML = 'We are from Moby word';
-}) ;
-
-button.addEventListener('click', () => {
-  const heading = document.querySelector('.article__heading--js');
-  heading.innerHTML = 'We left our world';
-  console.log(heading.classList.contains('article__heading'));
-}) ;
-
 const hamburger = document.querySelector('.hamburger--js')
 
 hamburger.addEventListener('click', () => {
   const nav = document.querySelector('.navigation--js');
   nav.classList.toggle('navigation--open')
 })
+
 
